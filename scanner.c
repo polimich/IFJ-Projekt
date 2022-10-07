30f8ee064b9e1572224b62ae5a515e2ca5fba6c3
Michael Polivka <michael.polivka1@gmail.com>
2022-10-07 11:35:38 +0200

    scanner.c - testovani lexeru

--- scanner.c
+++ scanner.c
@@ -14,6 +14,7 @@
 #include <ctype.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef enum {
     Start,
@@ -40,7 +41,7 @@ typedef enum {
     Comment,
     LineComment,
     BlockComment,
-    String,
+    StringLit,
     StringEsc,
     Var0,
     VarId,
@@ -52,46 +53,46 @@ FsmState transition(FsmState in, char edge)
 {
     switch (in) {
     case Start:
-        if (isalpha(edge) || edge == '_')
+        if (isalpha(edge) || edge == "_")
             return Identifier;
         if (isdigit(edge))
             return Number;
-        if (edge == ';')
+        if (edge == ";")
             return Semicolon;
-        if (edge == ',')
+        if (edge == ",")
             return Comma;
-        if (edge == '(')
+        if (edge == "(")
             return LPar;
-        if (edge == ')')
+        if (edge == ")")
             return RPar;
-        if (edge == '\\')
+        if (edge == "\\")
             return Div;
-        if (edge == '*')
+        if (edge == "*")
             return Mul;
-        if (edge == '+')
+        if (edge == "+")
             return Plus;
-        if (edge == '-')
+        if (edge == "-")
             return Minus;
-        if (edge == '=')
+        if (edge == "=")
             return Assign;
-        if (edge == '>')
+        if (edge == ">")
             return Greater;
-        if (edge == '<')
+        if (edge == "<")
             return Lesser;
-        if (edge == '/')
+        if (edge == "/")
             return Comment;
-        if (edge == '\"')
-            return String;
-        if (edge == '$')
+        if (edge == "\"")
+            return StringLit;
+        if (edge == "$")
             return Var0;
 
     //Number state
     case Number:
         if (isdigit(edge))
             return Number;
-        else if (edge == '.')
+        else if (edge == ".")
             return Point;
-        else if (edge == 'e' || edge == 'E')
+        else if (edge == "e" || edge == "E")
             return NumberExponent;
         return Error;
     case Point:
@@ -101,13 +102,13 @@ FsmState transition(FsmState in, char edge)
     case NumberDouble:
         if (isdigit(edge))
             return NumberDouble;
-        else if (edge == 'e' || edge == 'E')
+        else if (edge == "e" || edge == "E")
             return NumberExponent;
         return Error;
     case NumberExponent:
         if (isdigit(edge))
             return NumberExponentFinal;
-        else if (edge == '+' || edge == '-')
+        else if (edge == "+" || edge == "-")
             return NumberExponentSign;
         return Error;
     case NumberExponentSign:
@@ -121,17 +122,17 @@ FsmState transition(FsmState in, char edge)
 
     //Identifier or Keyword
     case Identifier:
-        if (isalpha(edge) || edge == '_')
+        if (isalpha(edge) || edge == "_")
             return Identifier;
         return Error;
 
     case Var0:
-        if (isalpha(edge) || edge == '_')
+        if (isalpha(edge) || edge == "_")
             return VarId;
         return Error;
 
     case VarId:
-        if (isalnum(edge) || edge == '_')
+        if (isalnum(edge) || edge == "_")
             return VarId;
         return Error;
 
@@ -146,24 +147,24 @@ FsmState transition(FsmState in, char edge)
     case Minus:
 
     case Assign:
-        if (edge == '=')
+        if (edge == "=")
             return Equals;
         return Assign;
 
     case Greater:
-        if (edge = '=')
+        if (edge = "=")
             return GreaterEven;
         return Greater;
 
     case Lesser:
-        if (edge == '=')
+        if (edge == "=")
             return LesserEven;
         return Lesser;
 
     case Comment:
-        if (edge == '/')
+        if (edge == "/")
             return LineComment;
-        else if (edge == '*')
+        else if (edge == "*")
             return BlockComment;
     }
 }
@@ -182,7 +183,7 @@ typedef struct {
         MUL,
         PLUS,
         MINUS,
-        ASSING,
+        ASSIGN,
         EQUALS,
         GREATER,
         GREATER_E,
@@ -190,29 +191,106 @@ typedef struct {
         LESSER_E,
         L_COMMENT,
         B_COMMENT,
-        STRING,
+        STRING_LIT,
         VARID,
+        LEX_EOF
     } kind;
-    union {
-        char* String;
-        int value;
-        int symlab_index;
-    };
+    size_t data;
 
 } Lexeme;
 
-Lexeme make_lexeme();
+//todo dynamicky
+char a[2048] = {0};
+char *pool_startp = &a[0];
+
+
+
+
+
+Lexeme make_lexeme(FsmState final, char* data  ) {
+  switch(final) {
+        case Identifier: return (Lexeme) {.kind=IDENTIFIER, .data = data - a};
+        case Number: return (Lexeme) {.kind=NUM, .data = data - a};
+        case NumberDouble: return (Lexeme) {.kind=NUM_DOUBLE, .data = data - a};
+        case NumberExponentFinal: return (Lexeme) {.kind=NUM_EXP, .data = data - a};
+        case Semicolon: return (Lexeme) {.kind=SEMICOLON};
+        case Comma: return (Lexeme) {.kind=COMMA};
+        case LPar: return (Lexeme) {.kind=LPAR};
+        case RPar: return (Lexeme) {.kind=RPAR};
+        case Div: return (Lexeme) {.kind=DIV};
+        case Mul: return (Lexeme) {.kind=MUL};
+        case Plus: return (Lexeme) {.kind=PLUS};
+        case Minus: return (Lexeme) {.kind=MINUS};
+        case Assign: return (Lexeme) {.kind=ASSIGN};
+        case Equals: return (Lexeme) {.kind=EQUALS};
+        case Greater: return (Lexeme) {.kind=GREATER};
+        case GreaterEven: return (Lexeme) {.kind=GREATER_E};
+        case Lesser: return (Lexeme) {.kind=LESSER};
+        case LesserEven: return (Lexeme) {.kind=LESSER_E};
+        case LineComment: return (Lexeme) {.kind=L_COMMENT};
+        case BlockComment: return (Lexeme) {.kind=B_COMMENT};
+        case StringLit: return (Lexeme) {.kind=STRING_LIT, .data = data - a};
+        case VarId: return (Lexeme) {.kind=STRING_LIT, .data = data - a};
+        case Error:
+          exit(1);
+  }
+}
 
 Lexeme get_lexeme()
 {
-    char edge;
+    int edge;
     FsmState now = Start;
+    char* lexeme_text = pool_startp;
     while (true) {
         edge = getchar();
+        if (edge == EOF) {
+          if (now == Start) {
+            return (Lexeme){.kind=LEX_EOF};
+          }
+          return make_lexeme(now, lexeme_text);
+        }
         FsmState next = transition(now, edge);
         if (next == Error) {
             fungetc(edge, stdin);
-            return make_lexeme(/*todo*/);
+            pool_startp++;
+            return make_lexeme(now, lexeme_text);
         }
+        *(pool_startp++) = edge;
+        now = next;
     }
+}
+
+char * str_lexeme(Lexeme in) {
+  switch(in.kind) {
+    case LEX_EOF: return "EOF";
+    case IDENTIFIER: a + in.data;
+    case NUM: return a + in.data;
+    case NUM_DOUBLE: return a + in.data;
+    case NUM_EXP: return a + in.data;
+    case SEMICOLON: return ";";
+    case COMMA: return ",";
+    case LPAR: return "(";
+    case RPAR: return ")";
+    case DIV: return "\\";
+    case MUL: return "*";
+    case PLUS: return "+";
+    case MINUS: return "-";
+    case ASSIGN: return "=";
+    case EQUALS: return "==";
+    case GREATER: return ">";
+    case GREATER_E: return ">=";
+    case LESSER: return "<";
+    case LESSER_E: return "<=";
+    case L_COMMENT: return "//";
+    case B_COMMENT: return "Block";
+    case STRING_LIT: return a + in.data;
+    case VARID: return a + in.data;
+  }
+}
+
+int main () {
+  Lexeme l = {0};
+  while (l.kind != LEX_EOF) {
+    l = get_lexeme();
+  }
 }
