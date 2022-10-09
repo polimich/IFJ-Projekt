4c5f9570e29513d1a3301fdb9a35f60269963890
Prokop <prokop.schield@protonmail.com>
2022-10-09 11:15:41 +0200

    feat(scanner.h): add header and imports

--- scanner.h
+++ scanner.h
@@ -1,3 +1,22 @@
+//////////////////////////////////////////////////////////
+//                                                      //
+//    Implementace překladače jazyka IFJ22              //
+//                                                      //
+//    Autoři: xlukas18, xmedri01, xpoliv07, xschie03    //
+//                                                      //
+//    Implementace scanner.h: xschie03      			//
+//    Datum: 9. 10. 2022                                //
+//                                                      //
+//    Licence: GNU GPL v3, nebo pozdější                //
+//                                                      //
+//////////////////////////////////////////////////////////
+
 #ifndef __SCANNER_H__
+#define __SCANNER_H__
+
+#include <ctype.h>
+#include <stdbool.h>
+
+#include "./singleton.h"
 
 #endif
