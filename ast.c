fd51e07b2e8e5322f093fa47bfbacedec2d26428
Prokop <prokop.schield@protonmail.com>
2022-10-17 13:48:00 +0200

    feat(ast.*): define ast_*_t

---
+++ ast.c
@@ -0,0 +1,14 @@
+//////////////////////////////////////////////////////////
+//                                                      //
+//    Implementace překladače jazyka IFJ22              //
+//                                                      //
+//    Autoři: xlukas18, xmedri01, xpoliv07, xschie03    //
+//                                                      //
+//    Implementace ast.c: xschie03                      //
+//    Datum: 17. 10. 2022                               //
+//                                                      //
+//    Licence: GNU GPL v3, nebo pozdější                //
+//                                                      //
+//////////////////////////////////////////////////////////
+
+#include "./ast.h"
