29c5212b082a51b043db91b015b41b9fd381a0ac
Prokop <prokop.schield@protonmail.com>
2022-10-19 18:01:32 +0200

    refactor(symbol.h): add details to symbol_t

--- symbol.h
+++ symbol.h
@@ -5,7 +5,7 @@
 //    Autoři: xlukas18, xmedri01, xpoliv07, xschie03    //
 //                                                      //
 //    Implementace symbol.h: xschie03                   //
-//    Datum: 11. 10. 2022                               //
+//    Datum: 11. 10. 2022 - 19. 10. 2022                //
 //                                                      //
 //    Licence: GNU GPL v3, nebo pozdější                //
 //                                                      //
@@ -14,21 +14,31 @@
 #ifndef __SYMBOL_H__
 #define __SYMBOL_H__
 
+#include <stdbool.h>
+
 #include "./singleton.h"
 
 typedef enum symbol_type {
     symbol_type_undefined = 1,
     symbol_type_function_identifier,
-    symbol_type_global_variable_identifier,
-    symbol_type_local_variable_identifier,
+    symbol_type_constant,
+    symbol_type_global_variable,
+    symbol_type_local_variable,
     symbol_type_operator,
 } symbol_type_t;
 
 typedef struct symbol_t {
     symbol_type_t type;
-    singleton_t* value;
+    singleton_t* str;
 
     long int line_number;
+
+    singleton_t* constant_type;
+    int constant_value_int;
+    float constant_value_float;
+
+    bool variable_is_declared;
+    bool variable_has_value;
 
     // next entry in symtable
     struct symbol_t* next;
