4358875d95b44e90b4aeb6728bffe2612c17cdbe
Prokop <prokop.schield@protonmail.com>
2022-10-19 17:53:09 +0200

    feat(error.c): implement throw_warning and throw_error

---
+++ error.c
@@ -0,0 +1,62 @@
+//////////////////////////////////////////////////////////
+//                                                      //
+//    Implementace překladače jazyka IFJ22              //
+//                                                      //
+//    Autoři: xlukas18, xmedri01, xpoliv07, xschie03    //
+//                                                      //
+//    Implementace error.c: xschie03                    //
+//    Datum: 19. 10. 2022                               //
+//                                                      //
+//    Licence: GNU GPL v3, nebo pozdější                //
+//                                                      //
+//////////////////////////////////////////////////////////
+
+#include "./error.h"
+#include "./varstring.h"
+
+#define COLOR_RED "\x1b[31m"
+#define COLOR_ORANGE "\x1b[33m"
+#define COLOR_YELLOW "\x1b[93m"
+#define COLOR_RESET "\x1b[0m"
+
+int CURRENT_RETURN_CODE = 0;
+
+int get_current_error_code()
+{
+    return CURRENT_RETURN_CODE;
+}
+
+inline void print_error(int error_code, const char* hfmt, const char* fmt, va_list args)
+{
+    varstring_t* errmsg = varstring_init();
+
+    fprintf(errmsg->stream, hfmt, error_code);
+
+    vfprintf(errmsg->stream, fmt, args);
+
+    fprintf(errmsg->stream, COLOR_RESET);
+
+    fprintf(stderr, "%s\n", varstring_destroy(errmsg)->strval);
+}
+
+void throw_warning(int error_code, const char* fmt, ...)
+{
+    if (CURRENT_RETURN_CODE == 0) {
+        CURRENT_RETURN_CODE = error_code;
+    }
+
+    va_list args;
+    va_start(args, fmt);
+    print_error(error_code, (COLOR_ORANGE "Warning(" COLOR_YELLOW "%d" COLOR_ORANGE "): " COLOR_YELLOW), fmt, args);
+    va_end(args);
+}
+
+void throw_error(int error_code, const char* fmt, ...)
+{
+    va_list args;
+    va_start(args, fmt);
+    print_error(error_code, (COLOR_RED "  Error(" COLOR_YELLOW "%d" COLOR_RED "): " COLOR_YELLOW), fmt, args);
+    va_end(args);
+
+    exit(error_code);
+}
