130b48b9e331c4cdb0be402c3da9e797cfdf6015
Prokop <prokop.schield@protonmail.com>
2022-10-24 11:34:19 +0200

    feat(main.h): create some main.h

---
+++ main.h
@@ -0,0 +1,21 @@
+//////////////////////////////////////////////////////////
+//                                                      //
+//    Implementace překladače jazyka IFJ22              //
+//                                                      //
+//    Autoři: xlukas18, xmedri01, xpoliv07, xschie03    //
+//                                                      //
+//    Implementace main.h: xschie03                     //
+//    Datum: 24. 10. 2022                               //
+//                                                      //
+//    Licence: GNU GPL v3, nebo pozdější                //
+//                                                      //
+//////////////////////////////////////////////////////////
+
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+#include "./error.h"
+#include "./memory.h"
+#include "./scanner.h"
+
+#endif
