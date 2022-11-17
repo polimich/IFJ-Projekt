a8da609871ceddb053261223145c1676f3016455
Prokop <prokop.schield@protonmail.com>
2022-11-17 14:06:18 +0100

    chore(parser.h): remove old code

--- parser.h
+++ parser.h
@@ -19,20 +19,8 @@
 #include "./ast.h"
 #include "./utf8.h"
 
-typedef enum parser_read_node_until {
-    parser_read_node_until_leaf,
-    parser_read_node_until_muldiv,
-    parser_read_node_until_addsubcon,
-    parser_read_node_until_lsgr,
-    parser_read_node_until_strict_equals,
-    parser_read_node_until_comma,
-    parser_read_node_until_semi,
-    parser_read_node_until_block_end,
-    parser_read_node_entirely,
-} parser_read_node_until_t;
-
 void parser_init();
-ast_node_t* parser_read_node(parser_read_node_until_t read_until, utf8_readstream_t* input);
-ast_node_t* parser_fill_node(ast_node_t* node, parser_read_node_until_t read_until, utf8_readstream_t* input);
+
+ast_node_t* parser(utf8_readstream_t* input);
 
 #endif
