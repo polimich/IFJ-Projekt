22e7afe20fdae027046173ded7ac40a4b4643eb1
Prokop <prokop.schield@protonmail.com>
2022-11-25 20:41:57 +0100

    test(main.c): update testing main()

--- main.c
+++ main.c
@@ -5,13 +5,36 @@
 //    Autoři: xlukas18, xmedri01, xpoliv07, xschie03    //
 //                                                      //
 //    Implementace main.c: xschie03                     //
-//    Datum: 24. 10. 2022                               //
+//    Datum: 24. 10. 2022 - 24. 11. 2022                //
 //                                                      //
 //    Licence: GNU GPL v3, nebo pozdější                //
 //                                                      //
 //////////////////////////////////////////////////////////
 
 #include "./main.h"
+
+void print_ast_node(ast_node_t* node)
+{
+    if (node->leaf) {
+        printf("%s", node->leaf->symbol->str->strval);
+        if (node->leaf->call_parameters) {
+            printf("(\x1b[B");
+            for (size_t i = 0; i < node->leaf->call_parameters->size; ++i) {
+                if (i)
+                    printf(",");
+
+                print_ast_node(node->leaf->call_parameters->parameters[i]->node);
+            }
+            printf("\x1b[A)");
+        }
+    } else {
+        printf(" ( \x1b[B");
+        print_ast_node(node->left);
+        printf(" %s ", node->op->str->strval);
+        print_ast_node(node->right);
+        printf(" \x1b[A) ");
+    }
+}
 
 int main(const int argc, const char* argv[])
 {
@@ -30,6 +53,9 @@ int main(const int argc, const char* argv[])
 
             fprintf(stdout, "%d\t%d\t%s\n", counter, line, token->strval);
         }
+    } else /*if ((argc > 1) && (strcmp(argv[1], "--parser") == 0)) */ {
+
+        print_ast_node(parser(utf8_stdin));
     }
 
     mem_clean();
