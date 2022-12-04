16a3523f8eacfa0bfc22d9df5153860c7c3fc2b1
Michael Polivka <michael.polivka1@gmail.com>
2022-12-04 21:55:15 +0100

    chore(generator.h): h file

--- generator.h
+++ generator.h
@@ -14,23 +14,41 @@
 #ifndef __GENERATOR_H__
 #define __GENERATOR_H__
 
+#include "./ast.h"
+#include "./label.h"
 #include <stdio.h>
 
-#include "./ast.h"
+#define __GEN_CREST__ data, output
+#define __GEN_DREST__ generator_data_t *data, FILE *output
 
-void generator_print_symbol(symbol_t* symbol, FILE* output);
-void generator_print_leaf(ast_leaf_t* leaf, FILE* output);
-void generator_print_statement(ast_node_t* node, FILE* output);
-void generator_print_conditional(ast_conditional_t* conditional, FILE* output);
-void generator_print_loop(ast_loop_t* loop, FILE* output);
-void generator_print_block_item(ast_block_item_t* item, FILE* output);
-void generator_print_block(ast_block_t* block, FILE* output);
-void generator_print_call_parameter(ast_call_parameter_t* param, FILE* output);
-void generator_print_call_parameter_list(ast_call_parameter_list_t* list, FILE* output);
-void generator_print_parameter(ast_parameter_t* param, FILE* output);
-void generator_print_parameter_list(ast_parameter_list_t* list, FILE* output);
-void generator_print_function(ast_function_t* fn, FILE* output);
-void generator_print_function_list(ast_function_list_t* list, FILE* output);
+typedef struct generator_data_t {
+    bool add_generated;
+    bool sub_generated;
+    bool mul_generated;
+    bool div_generated;
+} generator_data_t;
+
+void generator_print_constant(symbol_t* constant, __GEN_DREST__);
+void generator_print_local_variable(symbol_t* local_variable, __GEN_DREST__);
+void generator_print_function_call(ast_leaf_t* function, __GEN_DREST__);
+void generator_print_expression(ast_node_t* node, __GEN_DREST__);
+void generator_print_assignment(ast_node_t* node, __GEN_DREST__);
+void generator_print_operator(symbol_t* op, __GEN_DREST__);
+void generator_print_return(ast_node_t* node, __GEN_DREST__);
+
+void generator_print_symbol(symbol_t* symbol, __GEN_DREST__);
+void generator_print_leaf(ast_leaf_t* leaf, __GEN_DREST__);
+void generator_print_statement(ast_node_t* node, __GEN_DREST__);
+void generator_print_conditional(ast_conditional_t* conditional, __GEN_DREST__);
+void generator_print_loop(ast_loop_t* loop, __GEN_DREST__);
+void generator_print_block_item(ast_block_item_t* item, __GEN_DREST__);
+void generator_print_block(ast_block_t* block, __GEN_DREST__);
+void generator_print_call_parameter(ast_call_parameter_t* param, __GEN_DREST__);
+void generator_print_call_parameter_list(ast_call_parameter_list_t* list, __GEN_DREST__);
+void generator_print_parameter(ast_parameter_t* param, __GEN_DREST__);
+void generator_print_parameter_list(ast_function_t* function, __GEN_DREST__);
+void generator_print_function(ast_function_t* fn, __GEN_DREST__);
+void generator_print_function_list(ast_function_list_t* list, __GEN_DREST__);
 
 void generator(ast_function_list_t* list, FILE* output);
 
